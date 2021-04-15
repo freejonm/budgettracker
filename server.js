d96e4e6dfed583a0efe85a0bbe8a3fb31b2408a4
Jon Free <freejonm@gmail.com>
2021-04-15 11:05:03 -0400

    trying to fix  deploy bug

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.PORT || "mongodb://localhost/myFirstDatabase", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/myFirstDatabase", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
