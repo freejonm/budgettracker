ee40328ff466a54816d59182eb0d5e66f0cafc72
Jon Free <freejonm@gmail.com>
2021-04-15 10:04:41 -0400

    adding functionality to db.js

--- public/db.js
+++ public/db.js
@@ -28,4 +28,39 @@ function saveRecord(record){
     const store = transaction.objectStore("pending");
 
     store.add(record);
+
+    let nameEl = document.querySelector("#t-name");
+    let amountEl = document.querySelector("#t-amount");
+
+    nameEl.value = "";
+    amountEl.value = "";    
+};
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    const store = transaction.objectStore("pending");
+
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(gatAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+
+                const store = transaction.objectStore("pending");
+
+                store.clear();
+            });
+        }
+    }
 }
